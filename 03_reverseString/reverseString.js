1bf2519b5f077fcf06aeb6c2bad251ce7956b7db
glitter545 <bobbybroken992@gmail.com>
2022-12-24 23:34:01 -0500

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string) {
+   
+    let word = string;
+    let reverse = "";
 
-};
+    for (let i = string.length -1; i >= 0; i--) {
+        reverse += word[i]; 
+    }
+
+    return reverse;
+
+}; reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//count the letters 
+//loop til the last letter 
+//put string in an array // string is already an array, by default
+//countdown from last to first letter
+//store in a new variable
+//convert to string
+
