928ea13cbf319678d699b540fe2a30619ba72238
glitter545 <bobbybroken992@gmail.com>
2022-12-24 23:08:37 -0500

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,31 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
+    let word = "";
+   //loop from 0 to num
+
+
+   if(num <0){
+    word = "ERROR";
+    return word;
+    }
+   
+   for (let i=0; i < num; i++) {
+      word += string;
+        
+   }
+   
+
+
+return word;
+}; 
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+//1.store in a variable
+//2.loop through num, add to variable
+
+//make a repeat code block
+//test the condition
+
