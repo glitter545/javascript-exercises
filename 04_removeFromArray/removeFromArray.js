5d5952bff78c911b3aa70aebafadc13366f78d78
glitter545 <bobbybroken992@gmail.com>
2023-01-01 06:35:49 -0500

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...moreArgs) {
 
-};
+     return arr.filter(item => !moreArgs.includes(item));
+
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
