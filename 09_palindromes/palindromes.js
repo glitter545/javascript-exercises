e299aae62e5e55c2982500d7e24e18c72efb8708
glitter545 <bobbybroken992@gmail.com>
2023-01-02 10:27:54 -0500

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,53 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    
+    let words = word.toLowerCase();
+    let originalArray = [];
 
-};
+    for (let x of words) {
+        if (!(x.includes(" ") || x.includes("!") ||
+         x.includes(".") || x.includes(","))) {
+            originalArray.push(x);
+        }
+    };
 
+    let copy = []; 
+    // copying the original array into a new one
+    for (let i=0;i<originalArray.length;i++) {
+        copy.push(originalArray[i]);
+    }
+
+    let reversed = copy.reverse();
+    let truthy = [];
+
+    for (let i =0;i<originalArray.length;i++) {
+       if (originalArray[i] === reversed[i]) {
+        truthy.push('yes');
+       } 
+    }
+
+    if (originalArray.length === truthy.length) {
+        return true;
+    } else {
+        return false;
+    }
+
+}; 
 // Do not edit below this line
 module.exports = palindromes;
+
+//loop through every letter, store in an array
+//use reverse method
+//convert back to string
+//needs to ignore spaces
+//compare word with new array
+
+
+// let originalArray = ['r' , 'c', 'e'];
+// let copy = []; //effected
+// for (let i=0;i<originalArray.length;i++) {
+//     copy.push(originalArray[i]);
+// }
+
+// let reversed = copy.reverse();  //contagious
+// console.log(originalArray);
+// console.log(reversed);
