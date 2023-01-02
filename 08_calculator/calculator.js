5670697b044b6f827176f4776c8c4a670fb982c8
glitter545 <bobbybroken992@gmail.com>
2023-01-02 09:10:01 -0500

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,58 @@
-const add = function() {
+const add = function(num1,num2) {
+	return num1+num2;
+};
+
+const subtract = function(num1,num2) {
+	return num1-num2;
+};
+  // array as a parameter
+const sum = function(arr) {
+
+  return arr.reduce((accumulator,currentValue) => accumulator +
+  currentValue , 0);
+
+};
+
+const multiply = function(arr) {
+
+  return arr.reduce((accumulator,currentValue) => accumulator *
+  currentValue );
+};
+
+const power = function(num1,num2) {
+	return num1 ** num2;
+};
+
+const factorial = function(num) {
+
+  // 0! = 1
+  // 1! = 1
+  // 2! = 2
+  // 3! = 6
+  // 4! = 24     num=factorial       factorial/num = numberBefore
+
+  let arr = [];
+  if (num === 0) {
+    return 1;
+  }
+  for (let i = num; i >=1; i--) {
+    arr.push(i);
+  }
+
+
+  return arr.reduce((accumulator,currentValue) => accumulator * currentValue);
+
 	
 };
 
-const subtract = function() {
-	
-};
+//get factorials
+//divide
 
-const sum = function() {
-	
-};
+//define factorials
+//loop
+//add
+//0?
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
