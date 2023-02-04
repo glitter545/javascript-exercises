f1aa837bf0a2e6dbdd60a56f2297918e9e7af7bb
glitter545 <bobbybroken992@gmail.com>
2023-02-04 06:08:26 -0500

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((empty,item) => {
+        let titles = item.title;
+      empty.push(titles);
+      return empty;
+    },[]);
 };
 
 // Do not edit below this line
