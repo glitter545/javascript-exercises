1a1b3f5e88f772a1ebb672c782f5a44f83a7cb4a
glitter545 <bobbybroken992@gmail.com>
2023-02-05 08:14:08 -0500

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((personOne,personTwo) => {
+        let ageOne = personOne.yearOfDeath - personOne.yearOfBirth;
+        let ageTwo = personTwo.yearOfDeath - personTwo.yearOfBirth;
+        const today = new Date();
+        const year = today.getFullYear();
 
-};
-
+        if (isNaN(ageOne)) {
+            ageOne = year - personOne.yearOfBirth;
+          }
+          
+        if(ageTwo > ageOne) {
+            return personTwo;
+        }
+        return personOne;
+          
+    });
+    
+}
 // Do not edit below this line
 module.exports = findTheOldest;
+
